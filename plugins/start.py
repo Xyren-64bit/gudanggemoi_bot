3cff2891aa2ada229f1eeb9c14f324b5ade0727b
Sofiana hanifah Aprilia <96537396+Xyren-64bit@users.noreply.github.com>
2024-08-01 15:05:44 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -237,6 +237,54 @@ Unsuccessful: <code>{unsuccessful}</code></b>"""
         await msg.delete()
 
 
+@Bot.on_message(filters.command("bc") & filters.user(ADMINS))
+async def send_text(client: Bot, message: Message):
+    if message.reply_to_message:
+        query = await query_msg()
+        broadcast_msg = message.reply_to_message
+        total = 0
+        successful = 0
+        blocked = 0
+        deleted = 0
+        unsuccessful = 0
+
+        pls_wait = await message.reply(
+            "<code>Broadcasting Message Tunggu Sebentar...</code>"
+        )
+        for row in query:
+            chat_id = int(row[0])
+            if chat_id not in ADMINS:
+                try:
+                    await broadcast_msg.copy(chat_id, protect_content=PROTECT_CONTENT)
+                    successful += 1
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    await broadcast_msg.copy(chat_id, protect_content=PROTECT_CONTENT)
+                    successful += 1
+                except UserIsBlocked:
+                    await delete_user(chat_id)
+                    blocked += 1
+                except InputUserDeactivated:
+                    await delete_user(chat_id)
+                    deleted += 1
+                except BaseException:
+                    unsuccessful += 1
+                total += 1
+        status = f"""<b><u>Berhasil Broadcast</u>
+Jumlah Pengguna: <code>{total}</code>
+Berhasil: <code>{successful}</code>
+Gagal: <code>{unsuccessful}</code>
+Pengguna diblokir: <code>{blocked}</code>
+Akun Terhapus: <code>{deleted}</code></b>"""
+        return await pls_wait.edit(status)
+    else:
+        msg = await message.reply(
+            "<code>Gunakan Perintah ini Harus Sambil Reply ke pesan telegram yang ingin di Broadcast.</code>"
+        )
+        await asyncio.sleep(8)
+        await msg.delete()
+
+
 @Bot.on_message(filters.command("ping"))
 async def ping_pong(client, m: Message):
     start = time()
