d935995871155b8624d03499e299a316f816988c
Sofiana hanifah Aprilia <96537396+Xyren-64bit@users.noreply.github.com>
2024-08-01 14:53:42 +0800

    Update sql.py

--- database/sql.py
+++ database/sql.py
@@ -4,11 +4,11 @@ from sqlalchemy import TEXT, Column, Numeric, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from config import DB_URI
+from config import DB_URL
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(DB_URL, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
